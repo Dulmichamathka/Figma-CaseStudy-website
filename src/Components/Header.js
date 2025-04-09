843118389405faafaff60fb7d42689e6963782a7
Dulmi Chamathka <chamathkawijesekara23@gmail.com>
2025-04-10 00:07:59 +0530

    creating heading with tailwind css

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,31 @@
 import React from "react";
 
 const Header = () => {
-  return <div className="topic-font">Dulmi Chamathka</div>;
+  return (
+    <div className="flex justify-between p-5 align-middle shadow-md ">
+      <div className="flex text-2xl align-middle topic-font ">
+        <div className="mt-1">
+          Case <span className="text-blue-600"> Studies </span>
+        </div>
+        <img
+          src="general/light-bulb.png"
+          width={"35px"}
+          height={"20px"}
+          alt=""
+          className="rotate-12"
+        />
+      </div>
+
+      <div className="mt-1 mr-8 text-base font-semibold text-gray-500 topic-font">
+        <ul className="flex gap-8 sub-titles ">
+          <li>Works</li>
+          <li>Resume</li>
+          <li>LinkedIn</li>
+          <li>Git Hub</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
